b6fff6e4a47057647999cdee2a01e33c59728811
caitleigh <caitleigh27@gmail.com>
2020-03-19 19:20:15 -0700

    Updates

---
+++ precache-manifest.b9fdb945024b106f4d1e83d7cdff356a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f149feba7cf2b0af84343f42c76807e",
+    "url": "/gofetch/index.html"
+  },
+  {
+    "revision": "7b6059cb2524c3cef740",
+    "url": "/gofetch/static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "c76945c7e70b5ef5ae46",
+    "url": "/gofetch/static/js/2.2519ff78.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/gofetch/static/js/2.2519ff78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b6059cb2524c3cef740",
+    "url": "/gofetch/static/js/main.800f6b1f.chunk.js"
+  },
+  {
+    "revision": "5ce023f24e667c31f2fc",
+    "url": "/gofetch/static/js/runtime-main.a8c7aee3.js"
+  }
+]);
